c56c6053fa9f86b10db4a5f853646102e9ac46d9
aayan269 <ahmadaayan269@gmail.com>
2023-01-26 02:29:05 +0530

    added url3

--- src/shorturl.router.js
+++ src/shorturl.router.js
@@ -3,7 +3,7 @@ const validUrl = require('valid-url')
 const shortid = require('shortid')
 const Url = require('./shorturl.model')
 
-const baseUrl = 'http://localhost:8080'
+const baseUrl = 'https://shorturl-3mae.onrender.com'
 
 const app = express.Router()
 
